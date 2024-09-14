d96261f2d540780f370a121ec293a9ee36530dce
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-14 04:24:18 +0000

    findPath function

--- scripts/utils/path.js
+++ scripts/utils/path.js
@@ -1,1 +1,17 @@
-export function updatePath(path) {document.getElementById('path').innerText = `Path: ${path}`;}
+import { textNodes } from "../constants.js";
+
+export function updatePath(path) {
+    localStorage.path = path;
+    document.getElementById('path').innerText = `Path: ${path}`;
+}
+
+export function findPath(path) {
+    for (let index = 0; index < textNodes.length; index++) {
+        const element = textNodes[index];
+        
+        if (element.path == path)
+            return index;
+    }
+
+    return 1;
+}

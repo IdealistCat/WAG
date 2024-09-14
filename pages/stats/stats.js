db78e294953decb19953758d5e3ea81c50300501
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-09-14 04:57:13 +0000

    stats registering fix

--- pages/stats/stats.js
+++ pages/stats/stats.js
@@ -1,14 +1,15 @@
 import { state } from "../../scripts/utils/options.js";
 
-var paragraph = document.createElement('p');
-paragraph.innerHTML = 'Tester Bester can you tell me what rhymes with Bester :)';
-
 var data = [];
-data.push(`current control level: ${state.controlLV}`);
 data.push(`current path: ${localStorage.path}`);
+data.push(`current control level: ${(state.controlLV == undefined) ? 10 : state.controlLV}/10`);
+data.push(`resets: ${(localStorage.resets == undefined) ? 0 : localStorage.resets}`);
 
 for (let index = 0; index < data.length; index++) {
-    const element = array[index];
+    const element = data[index];
+
+    var paragraph = document.createElement('p');
+    paragraph.innerHTML = 'Tester Bester can you tell me what rhymes with Bester :)';
     paragraph.innerHTML = element;
     document.getElementsByClassName('container')[0].append(paragraph);
 }
